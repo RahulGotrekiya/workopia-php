d48eff7d1b3f22902c83b2ae1f4b3bc2fad241b1
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-03-18 22:47:12 +0530

    single listing named params

--- routes.php
+++ routes.php
@@ -3,3 +3,4 @@
 $router->get('/', 'controllers/home.php');
 $router->get('/listings', 'controllers/home.php');
 $router->get('/listings/create', 'controllers/listings/create.php');
+$router->get('/listing', 'controllers/listings/show.php');
